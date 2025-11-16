6f8a32fd0b0d8318b059778fcd7958cf9eafbf04
Mystie <91035618+MissMystie@users.noreply.github.com>
2025-11-16 16:46:47 -0500

    Scene transitions

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,8 +1,19 @@
 using Mystie.Core;
+using System.Collections;
 using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
+    public bool autoTransition;
+    public float delay = 11f;
+
+    public IEnumerator Start()
+    {
+        if (autoTransition) yield return new WaitForSeconds(delay);
+        else yield break;
+
+        NextLevelButton();
+    }
     public void NextLevelButton()
     {
         LevelManager.Instance.NextLevel();
